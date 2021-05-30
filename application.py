13c6c204f56354b546912ad89373678d00ad2f3e
Nick Reitano <nreitano95@gmail.com>
2021-05-29 20:17:22 -0600

    heroku deploy

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, json, request, redirect
 
-import database.db_connector as db
+# import database.db_connector as db
 
 import MySQLdb
 
@@ -11,38 +11,38 @@ db_connection = db.connect_to_database()
 
 @application.route('/', methods=('GET', 'POST'))
 def root():
-
+    
     return render_template("index.j2")
 
-@application.route('/database-test-create', methods=('GET', 'POST'))
-def dbCreateTest():
-    """ Create a new row in the Employees table """
+# @application.route('/database-test-create', methods=('GET', 'POST'))
+# def dbCreateTest():
+#     """ Create a new row in the Employees table """
 
-    # Get inputs from POST request and store as variables
-    if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
+#     # Get inputs from POST request and store as variables
+#     if request.method == 'POST':
+#         first_name = request.form['first_name']
+#         last_name = request.form['last_name']
         
-        # Set query to insert a row based on the form inputs
-        query = "INSERT INTO UsersTest (first_name, last_name) VALUES (%s, %s)"
-        cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(first_name, last_name))
-        results = cursor.fetchall()
+#         # Set query to insert a row based on the form inputs
+#         query = "INSERT INTO UsersTest (first_name, last_name) VALUES (%s, %s)"
+#         cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(first_name, last_name))
+#         results = cursor.fetchall()
 
-        return redirect('/db-test')
+#         return redirect('/db-test')
 
-    return render_template("databaseCreateTest.j2")
+#     return render_template("databaseCreateTest.j2")
 
-@application.route('/db-test', methods=('GET', 'POST'))
-def dbTest():
+# @application.route('/db-test', methods=('GET', 'POST'))
+# def dbTest():
 
-    query = "SELECT * FROM UsersTest ORDER BY last_name;"
+#     query = "SELECT * FROM UsersTest ORDER BY last_name;"
 
-    # Execute query to display rows
-    cursor = db.execute_query(db_connection=db_connection, query=query)
-    results = cursor.fetchall()
-    cursor.close()
+#     # Execute query to display rows
+#     cursor = db.execute_query(db_connection=db_connection, query=query)
+#     results = cursor.fetchall()
+#     cursor.close()
 
-    return render_template("databaseTest.j2", Users=results)
+#     return render_template("databaseTest.j2", Users=results)
 
 
 
